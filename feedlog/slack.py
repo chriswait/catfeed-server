fe1f6f03aa4012b2a4912484691b58c6c723d997
chriswait <chriswait91@gmail.com>
2016-07-06 18:38:44 +1000

    fix slack attachment, redirected urls

--- feedlog/slack.py
+++ feedlog/slack.py
@@ -2,17 +2,19 @@ import requests
 import json
 
 url = "https://hooks.slack.com/services/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"
-data = {
-    'username': "Lady",
-    'channel': '#lady',
-    "text": "I just got fed!",
-    "attachments": [
-        {
-            "text": "",
-            "image_url": "http://thecatapi.com/api/images/get?format=src&type=gif"
-        }
-    ]
-}
 def post_to_slack():
+    imgRequest = requests.post("http://thecatapi.com/api/images/get?format=src&type=gif")
+    imgRedirect = imgRequest.url
+    data = {
+        'username': "Lady",
+        'channel': '#lady',
+        "text": "I just got fed!",
+        "attachments": [
+            {
+                "text": "",
+                "image_url": imgRedirect,
+            }
+        ]
+    }
     request = requests.post(url, data=json.dumps(data))
     return request.text
